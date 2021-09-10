1ecbd632d9f4dc966b7f6157d2d3e7fece204587
Andrew <andrewmccann86@gmail.com>
2021-09-10 12:48:12 +0100

    error handling updated for app.py

--- capstone/final_code/app.py
+++ capstone/final_code/app.py
@@ -228,6 +228,30 @@ def create_app(test_config=None):
       "error": 422,
       "message": "unprocessable"
       }), 422
+  
+  @app.errorhandler(400)
+  def unprocessable(error):
+    return jsonify({
+        "success": False,
+        "error": 400,
+        "message": "Bad request"
+    }), 400
+
+  @app.errorhandler(404)
+  def unprocessable(error):
+    return jsonify({
+        "success": False,
+        "error": 404,
+        "message": "Not found"
+    }), 404
+  
+  @app.errorhandler(AuthError)
+  def auth_error(error):
+    return jsonify({
+        "success": False,
+        "error": error.status_code,
+        "message": error.error
+    }), error.status_code
 
   return app
 
